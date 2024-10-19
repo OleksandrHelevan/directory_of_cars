fb650f57d84c6d06e6c285a22d33773a667b9eff
OleksandrHelevan <oleksandrhelevan@gmail.com>
2024-10-20 00:45:58 +0300

    FINAL CODE!!!

--- Person.h
+++ Person.h
@@ -15,29 +15,21 @@ using namespace std;
 class Person {
 public:
     virtual ~Person() = default;
-
     virtual void write_to_file() = 0;
-
     virtual bool search() = 0;
 
     virtual void add_car() = 0;
-
     virtual void add_truck() = 0;
-
     virtual void add_bus() = 0;
 
     virtual void set_car() = 0;
-
     virtual void set_truck() = 0;
-
     virtual void set_bus() = 0;
 
     virtual void delete_vehicle() = 0;
 
     virtual void car_menu() = 0;
-
     virtual void truck_menu() = 0;
-
     virtual void bus_menu() = 0;
 
 };
--- Truck.cpp
+++ Truck.cpp
@@ -7,11 +7,7 @@ Truck::Truck() : Vehicle(), cargo_capacity{0} {}
 
 Truck::Truck(Engine &new_engine, int weigh, double consumption, int new_mileage, std::string &col, std::string &bran,
              std::string &mod, int new_year, std::string &loc, int new_price, int cargo) : cargo_capacity{cargo},
-                                                                                           Vehicle(new_engine, weigh,
-                                                                                                   consumption,
-                                                                                                   new_mileage, col,
-                                                                                                   bran, mod, new_year,
-                                                                                                   loc, new_price) {}
+             Vehicle(new_engine, weigh, consumption, new_mileage, col, bran, mod, new_year,loc, new_price) {}
 
 ostream &operator<<(ostream &os, const Truck &obj) {
     os << static_cast<const Vehicle &>(obj);
